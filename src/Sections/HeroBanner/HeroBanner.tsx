de672d7df00a4400389472a62233ff2b62ba8730
maxg02 <maxdanielgs@hotmail.com>
2025-09-02 16:44:48 -0400

    Setting fonts and banner initial layout

--- src/Sections/HeroBanner/HeroBanner.tsx
+++ src/Sections/HeroBanner/HeroBanner.tsx
@@ -1,20 +1,32 @@
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCircle } from "@fortawesome/free-solid-svg-icons";
 
 function HeroBanner() {
     return (
         <div className="app-container">
             <div className="h-dvh flex flex-col justify-center items-center gap-8">
-                <div>
-                    <h1>Max García</h1>
-                    <h2>Web Developer</h2>
+                <div className="text-center">
+                    <h1 className="text-[13vw]/8">Max García</h1>
+                    <span className="pretty-text text-[8.5vw] text-custom-red">Web Developer</span>
                 </div>
-                <nav>
-                    <ul className="flex gap-4 bg-red-300">
-                        <li>Home</li>
-                        <li>About</li>
-                        <li>Projects</li>
-                        <li>Contact</li>
-                    </ul>
+                <nav className="flex w-full justify-evenly">
+                    <div className="flex flex-col items-center gap-y-1">
+                        <p>Home</p>
+                        <FontAwesomeIcon icon={faCircle} className="opacity-25" />
+                    </div>
+                    <div>
+                        <p>About</p>
+                        <FontAwesomeIcon icon={faCircle} className="opacity-25" />
+                    </div>
+                    <div>
+                        <p>Projects</p>
+                        <FontAwesomeIcon icon={faCircle} className="opacity-25" />
+                    </div>
+                    <div>
+                        <p>Contact</p>
+                        <FontAwesomeIcon icon={faCircle} className="opacity-25" />
+                    </div>
                 </nav>
             </div>
         </div>
