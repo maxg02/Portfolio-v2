24f0581c49e997fb2d7f886c45429b890c3d0460
Maxg02 <maxdanielsanchezgarcia@gmail.com>
2025-09-03 06:22:53 -0400

    Refactor HeroBanner component to use a dynamic navigation bar

--- src/Sections/HeroBanner/HeroBanner.tsx
+++ src/Sections/HeroBanner/HeroBanner.tsx
@@ -1,8 +1,17 @@
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faCircle } from "@fortawesome/free-solid-svg-icons";
+import { faArrowDown, faCircle } from "@fortawesome/free-solid-svg-icons";
 
 function HeroBanner() {
+    const sections = ["Home", "About", "Projects", "Contact"];
+
+    const navBar = sections.map((section) => (
+        <div className="text-center" key={section}>
+            <p>{section}</p>
+            <FontAwesomeIcon icon={faCircle} className="opacity-25" />
+        </div>
+    ));
+
     return (
         <div className="app-container">
             <div className="h-dvh flex flex-col justify-center items-center gap-8">
@@ -10,24 +19,18 @@ function HeroBanner() {
                     <h1 className="text-[13vw]/8">Max García</h1>
                     <span className="pretty-text text-[8.5vw] text-custom-red">Web Developer</span>
                 </div>
-                <nav className="flex w-full justify-evenly">
-                    <div className="flex flex-col items-center gap-y-1">
-                        <p>Home</p>
-                        <FontAwesomeIcon icon={faCircle} className="opacity-25" />
-                    </div>
-                    <div>
-                        <p>About</p>
-                        <FontAwesomeIcon icon={faCircle} className="opacity-25" />
-                    </div>
-                    <div>
-                        <p>Projects</p>
-                        <FontAwesomeIcon icon={faCircle} className="opacity-25" />
-                    </div>
-                    <div>
-                        <p>Contact</p>
-                        <FontAwesomeIcon icon={faCircle} className="opacity-25" />
-                    </div>
-                </nav>
+                <nav className="flex w-screen justify-evenly sticky top-0">{navBar}</nav>
+                <div className="absolute bottom-4 text-center">
+                    <p className="opacity-50 mb-4 leading-5">
+                        Let's know each
+                        <br /> other
+                    </p>
+                    <FontAwesomeIcon
+                        icon={faArrowDown}
+                        className="text-custom-red animate-bounce"
+                        size={"3x"}
+                    />
+                </div>
             </div>
         </div>
     );
