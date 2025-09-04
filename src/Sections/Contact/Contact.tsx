1aa111ba18d20d9df6322b6016de99a274a22d4e
maxg02 <maxdanielgs@hotmail.com>
2025-09-04 16:30:25 -0400

    Create contact section

---
+++ src/Sections/Contact/Contact.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+import { HiOutlineMail } from "react-icons/hi";
+import { VscGithub } from "react-icons/vsc";
+import { PiLinkedinLogoBold } from "react-icons/pi";
+import "./styles.css";
+
+function Contact() {
+    return (
+        <div className="app-container h-dvh flex items-center justify-center flex-col gap-y-9">
+            <div className="grid grid-cols-2 grid-rows-2 w-fit gap-4 rotate-45">
+                <a href="mailto:maxdanielgs@hotmail.com" target="_blank" rel="noreferrer">
+                    <div className="contact-button hover:-translate-2 hover:bg-custom-red">
+                        <HiOutlineMail />
+                    </div>
+                </a>
+                <a href="https://github.com/maxg02" target="_blank" rel="noreferrer">
+                    <div className="contact-button hover:-translate-y-2 hover:translate-x-2 hover:bg-custom-red">
+                        <VscGithub />
+                    </div>
+                </a>
+                <a
+                    href="https://www.linkedin.com/in/max-daniel-garcia-sanchez-b9658a224"
+                    target="_blank"
+                    rel="noreferrer"
+                >
+                    <div className="contact-button hover:translate-y-2 hover:-translate-x-2 hover:bg-custom-red">
+                        <PiLinkedinLogoBold />
+                    </div>
+                </a>
+            </div>
+            <h1 className="text-[11vw] leading-2">Contact Me!</h1>
+        </div>
+    );
+}
+
+export default Contact;
